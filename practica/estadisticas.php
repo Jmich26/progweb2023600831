7bff278e0346e33594476aeac19444f4ba8e8d68
Jmich26 <jhoanamich26@gmail.com>
2025-03-27 01:02:49 -0600

    Min primera version

---
+++ practica/estadisticas.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calificaciones</title>
+    <style>
+        //Colocar estilos para la tabla
+    </style>
+</head>
+<body>
+
+    <h2>Tabla de Calificaciones</h2>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Apellidos</th>
+                <th>Nombre</th>
+                <th>Parcial 1</th>
+                <th>Parcial 2</th>
+                <th>Parcial 3</th>
+                <th>Promedio Final</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Pérez</td>
+                <td>Juan</td>
+                <td>8</td>
+                <td>9</td>
+                <td>8</td>
+                <td>9</td>
+            </tr>
+            <tr>
+                <td>López</td>
+                <td>María</td>
+                <td>7</td>
+                <td>8</td>
+                <td>8</td>
+                <td>8</td>
+            </tr>
+            <tr>
+                <td>García</td>
+                <td>Carlos</td>
+                <td>9</td>
+                <td>8</td>
+                <td>9</td>
+                <td>9</td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+</html>
